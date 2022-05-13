7539e78879e53d6c7e4471276e0a118d887121a6
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-13 23:10:06 +0200

    Budget pages have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './components/app/app.component';
 import { MainComponent } from './components/main/main.component';
 import { AboutComponent } from './components/about/about.component';
+import { ListComponent } from './components/budget/list/list.component';
+import { AddComponent } from './components/budget/add/add.component';
+import { EditComponent } from './components/budget/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    AboutComponent
+    AboutComponent,
+    ListComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
