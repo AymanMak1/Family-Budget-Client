6861b5877ef8b35a81069e61aaf348b58736de1f
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-20 01:32:12 +0200

    Working on adding budget service

--- src/app/components/budget/add-budget/add-budget.component.ts
+++ src/app/components/budget/add-budget/add-budget.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Budget } from 'src/app/budget';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-add-budget',
   templateUrl: './add-budget.component.html',
   styleUrls: ['./add-budget.component.css']
 })
-export class AddBudgetComponent implements OnInit {
+export class AddBudgetComponent{
+  budgets:any;
+  budget = new Budget();
+  constructor(private budgetService: BudgetService, private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onSave(budget: Budget) {
+    this.budgetService.add(budget).subscribe((data) => {
+      this.router.navigate(['/budgets']);
+    });
   }
-
 }
