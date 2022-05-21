9dd5173effa22450dfbb479fc65dea0d7e75e4f5
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-21 21:50:43 +0200

    Setting up the reactive budget form

--- src/app/components/budget-form/budget-form.component.ts
+++ src/app/components/budget-form/budget-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Budget } from 'src/app/budget';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'budget-form',
@@ -6,14 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-form.component.css']
 })
 export class BudgetFormComponent implements OnInit {
+  @Input() budget: Budget | undefined;
+  public budgetForm!: FormGroup;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
+    this.budgetForm = this.formBuilder.group({
+      title: [this.budget?.title, [Validators.required,Validators.minLength]],
+      amount:  [this.budget?.amount,[Validators.required]],
+      category:  [this.budget?.category,[Validators.required]],
+    })
   }
 
-  addBudget(){
-    console.log("Hello");
+  public onSubmit(e: any){
+    e.preventDefault();
+    console.log(this.budgetForm);
   }
+  public isInvalidAndTouchedOrDirty(name: string) {
+    return !this.budgetForm.get(name)?.valid && (this.budgetForm.get(name)?.touched || this.budgetForm.get(name)?.dirty);
+  }
+  public isValid(name: string){
+    return this.budgetForm.get(name)?.valid
+  }
+  /*saveBudget(){
+    this.budgetService.addNewBudget(this.budget).subscribe((data) => {
+      this.router.navigate(['/budgets']);
+    });
+  }*/
 
 }
