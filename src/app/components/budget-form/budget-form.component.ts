f5705b7276ff0469307db2e33039b5a349992d6c
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-22 02:16:26 +0200

    reusable budget form done

--- src/app/components/budget-form/budget-form.component.ts
+++ src/app/components/budget-form/budget-form.component.ts
@@ -1,16 +1,17 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Budget } from 'src/app/budget';
-import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'budget-form',
   templateUrl: './budget-form.component.html',
   styleUrls: ['./budget-form.component.css']
 })
-export class BudgetFormComponent implements OnInit {
+export class BudgetFormComponent implements OnInit, OnChanges {
   @Input() budget: Budget | undefined;
+  @Output() saveBudget: EventEmitter<Budget> = new EventEmitter<Budget>();
+
   public budgetForm!: FormGroup;
 
 
@@ -20,26 +21,30 @@ export class BudgetFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.budgetForm = this.formBuilder.group({
+      id: [this.budget?.id],
       title: [this.budget?.title, [Validators.required,Validators.minLength]],
       amount:  [this.budget?.amount,[Validators.required]],
       category:  [this.budget?.category,[Validators.required]],
     })
   }
 
-  public onSubmit(e: any){
-    e.preventDefault();
-    console.log(this.budgetForm);
+  public ngOnChanges(): void {
+      this.budgetForm = this.formBuilder.group({
+      id: [this.budget?.id],
+      title: [this.budget?.title, [Validators.required,Validators.minLength]],
+      amount:  [this.budget?.amount,[Validators.required]],
+      category:  [this.budget?.category,[Validators.required]],
+    })
   }
+  public onSubmit(){
+      this.saveBudget.emit(this.budgetForm.value);
+  }
+
   public isInvalidAndTouchedOrDirty(name: string) {
     return !this.budgetForm.get(name)?.valid && (this.budgetForm.get(name)?.touched || this.budgetForm.get(name)?.dirty);
   }
   public isValid(name: string){
     return this.budgetForm.get(name)?.valid
   }
-  /*saveBudget(){
-    this.budgetService.addNewBudget(this.budget).subscribe((data) => {
-      this.router.navigate(['/budgets']);
-    });
-  }*/
 
 }
