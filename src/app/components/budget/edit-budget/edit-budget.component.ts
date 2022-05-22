2791e1389b6ff0e0898f6053da39a36aeea81403
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-22 02:15:55 +0200

    edit budget form done

--- src/app/components/budget/edit-budget/edit-budget.component.ts
+++ src/app/components/budget/edit-budget/edit-budget.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Budget } from 'src/app/budget';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-edit-budget',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-budget.component.css']
 })
 export class EditBudgetComponent implements OnInit {
+  public budget: Budget | undefined;
+  constructor(
+    private budgetService: BudgetService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
-  constructor() { }
+  public ngOnInit(): void {
+    let id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.budgetService.get(id).subscribe((data: Budget) => {
+      this.budget = data;
+    });
+  }
 
-  ngOnInit(): void {
+  public onSaveBudget(budget: Budget): void{
+    this.budgetService.update(budget.id, budget).subscribe(() => {
+      this.router.navigate(['/budgets']);
+    });
   }
 
 }
