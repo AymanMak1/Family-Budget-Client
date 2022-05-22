b14ca733f17afce21c3097a283c12141e51912f3
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-22 02:16:55 +0200

    overall income and overall outcome on the main page

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  //public overall: any;
+  public overallIncome: number | undefined;
+  public overallOutcome: number | undefined;
 
-  constructor() { }
+  constructor(private budgetService: BudgetService, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.budgetService.getOverallIncomeAndOutcome().subscribe((data: any)=>{
+      //this.overall = data;
+      this.overallIncome= data['income'];
+      this.overallOutcome= data['outcome'];
+    })
   }
 
 }
