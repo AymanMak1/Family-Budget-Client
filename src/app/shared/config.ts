df1603e7607373727e273c2f4cb5876199c5c631
Ayman Makhoukhi <ayman.makhoukhi@gmail.com>
2022-05-22 02:18:02 +0200

    adding Overall Url endpoint to the config

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -2,7 +2,8 @@ export let configuration ={
   webApiEndPoint : "http://127.0.0.1:8000/",
   urls:{
     query:{
-      apiBudgetsUrl:'api/budgets'
+      apiBudgetsUrl:'api/budgets',
+      apiOverallUrl:'api/overall'
     }
   }
 }
